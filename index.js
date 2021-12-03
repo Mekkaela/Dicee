16de5bf49d0dae8c3f2442ef82fc8248811d0607
Mekka Ferreras <72678488+Mekkaela@users.noreply.github.com>
2021-12-03 21:26:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-window.addEventListener('load', function(event){
+document.addEventListener('load', function(event){
   diceReload();
 });
 
 function diceReload(){
   var player1 = Math.floor(Math.random() * 6) + 1;
   var player2 = Math.floor(Math.random() * 6) + 1;
-  console.log(player1);
-  console.log(player2);
   Winner(player1, player2);
   ReloadDice(player1, player2);
 }
 
 function Winner(player1, player2){
   if(player1 > player2){
-    console.log("Player 1 Wins");
     document.querySelector(".displayText").innerText = "🚩 Player 1 Wins!";
   }else if(player1 < player2){
-    console.log("Player 2 Wins");
     document.querySelector(".displayText").innerText = "Player 2 Wins! 🚩";
   }else {
-    console.log("Draw");
     document.querySelector(".displayText").innerText = "Draw!";
   }
 }
