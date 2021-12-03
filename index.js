dc5f968bbea9224ae5640150811571687613e7d2
Mekka Ferreras <72678488+Mekkaela@users.noreply.github.com>
2021-12-03 21:20:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+window.addEventListener('load', function(event){
+  diceReload();
+});
 
-var num1 = Math.floor(Math.random() * 6) + 1;
-var num2 = Math.floor(Math.random() * 6) + 1;
-
-document.querySelectorAll("img")[0].src = "images/dice" + num1 + ".png";
-document.querySelectorAll("img")[1].src = "images/dice" + num2 + ".png";
-
-var header = "Refresh Me";
-if (num1 > num2) {
-    header = "🚩 Player 1 Wins!";
-} else if (num2 > num1) {
-    header = "Player 2 Wins! 🚩";
-} else {
-    header = "Draw!";
+function diceReload(){
+  var player1 = Math.floor(Math.random() * 6) + 1;
+  var player2 = Math.floor(Math.random() * 6) + 1;
+  console.log(player1);
+  console.log(player2);
+  Winner(player1, player2);
+  ReloadDice(player1, player2);
 }
 
-document.querySelectorAll("h1")[0].innerHTML = header;
+function Winner(player1, player2){
+  if(player1 > player2){
+    console.log("Player 1 Wins");
+    document.querySelector(".displayText").innerText = "🚩 Player 1 Wins!";
+  }else if(player1 < player2){
+    console.log("Player 2 Wins");
+    document.querySelector(".displayText").innerText = "Player 2 Wins! 🚩";
+  }else {
+    console.log("Draw");
+    document.querySelector(".displayText").innerText = "Draw!";
+  }
+}
+
+function ReloadDice(player1, player2){
+  document.getElementById('img1').src = 'images/dice'+p1+'.png';
+  document.getElementById('img2').src = 'images/dice'+p2+'.png';
+}
+
